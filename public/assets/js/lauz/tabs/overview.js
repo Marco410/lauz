ea7e0edb1dfb19d0edd9e9e6b752227c89cbec66
Miguel Angel Landa <miguel.landa.villa@gmail.com>
2024-06-07 00:07:35 -0600

    Seleccion elementos js

--- public/assets/js/lauz/tabs/overview.js
+++ public/assets/js/lauz/tabs/overview.js
@@ -1,16 +1,30 @@
+const totalMfe = document.getElementById('totalNetPl2');
+const totalMae = document.getElementById('annualReturn2');
+const totalAvg_trade = document.getElementById('DrawDown2');
+const totalAvg_tradeDay = document.getElementById('profitFactor2');
+const totalComision = document.getElementById('avgWinLoss2');
+const totalSharpeRatio = document.getElementById('QTrades2');
+
+let accountsSelectGlobal2 = document.querySelector('select[name="accounts2"]');
+let initDateGlobal2 = document.querySelector('input[name="initDate2"]');
+let endDateGlobal2 = document.querySelector('input[name="endDate2"]');
+
+// let Instrument = document.querySelector('select[name="how_often_invest"]');
+
+
 function getMFE() {
     $.ajax({
         url: URLS.getmfe,
         type: "GET",
         dataType: "json",
         data: {
-            account: accountSelected,
-            initDate: initDate,
-            endDate: endDate,
-            user: user,
-            Market_pos : Market_pos,
-            Trade_Result : Trade_Result,
-            Instrument : Instrument,
+            account: accountsSelectGlobal2,
+            initDate: initDateGlobal2,
+            endDate: endDateGlobal2,
+            // user: user,
+            // Market_pos : Market_pos,
+            // Trade_Result : Trade_Result,
+            // Instrument : Instrument,
         },
         success: function (response) {
 
